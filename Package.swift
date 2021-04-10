1adcb209c0aa83926390c7ccc36267035ce4f11f
Gene Crucean <genecrucean@gmail.com>
2021-04-10 14:37:03 -0400

    Test dependencies

--- Package.swift
+++ Package.swift
@@ -26,7 +26,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Solana",
-            dependencies: []),
+            dependencies: [
+                "Base58Swift",
+                "ed25519swift"
+            ]),
         .testTarget(
             name: "SolanaTests",
             dependencies: ["Solana"]),
