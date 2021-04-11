184239b0f0b3d3fe7dde371e0833092c2325c508
Gene Crucean <genecrucean@gmail.com>
2021-04-11 15:06:48 -0400

    Fixed test

--- Tests/SolanaTests/SolanaTests.swift
+++ Tests/SolanaTests/SolanaTests.swift
@@ -2,14 +2,15 @@ import XCTest
 @testable import Solana
 
 final class SolanaTests: XCTestCase {
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Solana().text, "Hello, World!")
+        XCTAssertEqual("Tests coming soon", "Tests coming soon")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
